9b94b2024ab16603f8570ecb6c4b5512346f4b17
HiteshYaduvanshi <67088395+HiteshYaduvanshi@users.noreply.github.com>
2024-04-12 23:42:33 +0530

    Update cors policy server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const cors = require('cors')
 
 
 const corsOptions = {
-  origin: 'http://localhost:5173',
+  origin: 'https://employee-dashboard-eight.vercel.app/',
   methods: 'GET, POST, PUT, DELETE, PATCH, HEAD',
   credentials: true,
 }
